2422396efb90468ff4ac91b36055ab73e231e8fc
Loaye <jr.carstens00@gmail.com>
2017-05-12 15:59:18 -0700

    made a function that multiplies

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 'use strict';
+/////////////////////////////////////
+/* Problem 1 (this is your demo that we'll solve in class)
+Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
+"The sum of 4 and 7 is 11."
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
-function sum(a,b){
+// Write your code here
+function sum(a,b){//eslint-disable-line
   var mySum = a + b;
   var myMsg = 'The sum of ' + a + ' and ' + b + ' is ' + mySum + '.';
   return [mySum, myMsg];
 }
 
 sum();
+
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -18,8 +26,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+  var myMultiply = a * b;
+  var myMsg = 'The product of ' + a + ' and ' + b + ' is ' + myMultiply + '.';
+  return [myMultiply, myMsg];
 }
+
+multiply();
+
+testMultiply(5, 9);
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
